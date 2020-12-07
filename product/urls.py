9fc37e84aeb8368c2647940716b07e9811683016
= <tnbw.lab@gmail.com>
2020-12-07 22:49:52 +0700

    add product and owner relations

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views as product_views
 
 router = routers.DefaultRouter()
-router.register(r'product', product_views.ProductViewset)
+router.register(r'', product_views.ProductViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
